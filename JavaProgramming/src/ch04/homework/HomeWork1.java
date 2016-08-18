c236c070a5971a735e9ed18ff02de6b3b4491829
smilerici <smileric@naver.com>
2016-08-18 19:03:56 +0900

    3일차

--- JavaProgramming/src/ch04/homework/HomeWork1.java
+++ JavaProgramming/src/ch04/homework/HomeWork1.java
@@ -3,5 +3,12 @@ package ch04.homework;
 public class HomeWork1 {
 	public static void main(String[] args) {
 		
+		for(int i=0;i<100;i++) {
+			int num1 = (int)(Math.random()*6)+1;
+			int num2 = (int)(Math.random()*6)+1;
+			System.out.println(num1);
+			System.out.println(num2);
+			
+		}
 	}
 }
