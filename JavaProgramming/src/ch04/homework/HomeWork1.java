e913e653e193f7ae51b7e614ce309789b4bee79a
Administrator <Administrator@WIN7-20160122EM>
2016-08-18 19:56:24 +0900

    집에서

--- JavaProgramming/src/ch04/homework/HomeWork1.java
+++ JavaProgramming/src/ch04/homework/HomeWork1.java
@@ -2,13 +2,18 @@ package ch04.homework;
 
 public class HomeWork1 {
 	public static void main(String[] args) {
-		
-		for(int i=0;i<100;i++) {
-			int num1 = (int)(Math.random()*6)+1;
-			int num2 = (int)(Math.random()*6)+1;
-			System.out.println(num1);
-			System.out.println(num2);
-			
+		int sum=0; //주사위의 합이 저장될 변수 sum
+		for(int i=0;i<100;i++) { //주사위 두개를 100번 던진다. i가 0 부터 99 가 될때까지 블럭안의 값 반복 실행
+			int num1 = (int)(Math.random()*6)+1; // 0*6<=Math.random()*6 <1*6  => 0부터 6 이하의 실수난수 발생
+												 // 0*6<=(int)(Math.random()*6) <1*6 => 0부터 6이하의 정수 난수 발생
+												 // 0*6+1<=(int)(Math.random()*6)+1 <1*6+1 => 1,2,3,4,5,6 중 하나의 정수 발생
+												 //값을 int 형 변수 num1 에 저장
+			int num2 = (int)(Math.random()*6)+1; 
+			sum = num1+num2; //num1의 값과 num2의 값을 더한다.
+			if(sum==10) { //sum의 값이 10 일때 블럭안의 값을 출력
+				System.out.println("("+num1+","+num2+")"); //sum의 값이 10일때의 num1과 num2 출력
+			}
+
 		}
 	}
 }
--- JavaProgramming/src/ch04/homework/Homework2.java
+++ JavaProgramming/src/ch04/homework/Homework2.java
@@ -1,5 +1,14 @@
 package ch04.homework;
-
+	
 public class Homework2 {
-
+	public static void main(String[] args) {
+		
+		for(int x=1;x<=10;x++) {// int형 변수 x에 1부터 10까지의 값을 반복 저장
+			for(int y=1;y<=10;y++) {// int 형 변수 y에 1부터 10까지의 값을 반복 저장
+				if(4*x+5*y==60) { //4*x+5*y 값이 60 일때 블럭안의 값 출력
+					System.out.println("("+x+","+y+")");
+				}				
+			}
+		}
+	}
 }
