51a5b92ebd6e3fa77932160d1e3b66a33125aac6
Administrator <Administrator@WIN7-20160122EM>
2016-09-27 13:31:13 +0900

    20160927

---
+++ inflearn/src/com/javalec/exe/TestEx.java
@@ -0,0 +1,21 @@
+package com.javalec.exe;
+
+public class TestEx {
+	public static void main(String[] args) {
+		System.out.println("변수란 무엇이든 담을수 있는 주머니와 같은 녀셕 입니다.");
+		int i = 10;
+		int j = 20;
+		int h = 30;
+		
+		char c = 'A';
+		char d = 'a';
+		char e = 'B';
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(h);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
